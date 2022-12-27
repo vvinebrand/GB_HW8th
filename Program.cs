79003c4a6c36fb18a41852a3aba293310eca4bda
Viktoriia Nekrasova <vvinebrand@gmail.com>
2022-12-28 01:30:53 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 int programm;
 Boolean begin = true;
 
 while (begin)
 {
-    Console.WriteLine("\nЗадача 1. \nЗадача 2. \nЗадача 3. \nЗадача 4. \nЗадача 5. \nВведите 0 для выхода\n");
+    Console.WriteLine("\nЗадача 1. Сортировка строк массива \nЗадача 2. \nЗадача 3. \nЗадача 4. \nЗадача 5. \nВведите 0 для выхода\n");
     Console.Write("Введите номер задачи: ");
     programm = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
@@ -14,7 +15,37 @@ while (begin)
     {
         case 1:
 
-            
+            int m = InputNumbers("Введите m: ");
+            int n = InputNumbers("Введите n: ");
+            Console.WriteLine();
+
+            int[,] array = new int[m, n];
+            CreateArray(array);
+            WriteArray(array);
+
+            Console.WriteLine($"\nОтсортированный массив: \n");
+            OrderArrayLines(array);
+            WriteArray(array);
+
+            void OrderArrayLines(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        for (int k = 0; k < array.GetLength(1) - 1; k++)
+                        {
+                            if (array[i, k] < array[i, k + 1])
+                            {
+                                int temp = array[i, k + 1];
+                                array[i, k + 1] = array[i, k];
+                                array[i, k] = temp;
+                            }
+                        }
+                    }
+                }
+            }
+
             break;
 
         case 2:
@@ -39,5 +70,35 @@ while (begin)
         default:
             begin = false;
             break;
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10,99);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
 }
